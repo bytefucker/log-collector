1107ea8045f960a33c5cecfab3e877d4c276bf50
yihongzhi <yihongzhi@megvii.com>
2019-09-20 11:41:37 +0800

    构建脚本

--- build.sh
+++ build.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+ROOTPATH=$(pwd)
+
+echo "构建根目录:$ROOTPATH"
 
 echo "----------开始构建logAgent-------"
+cd $ROOTPATH/logAgent
+cp -R config bin/config
+go build -x -o bin/logAgent logAgent
+echo "----------构建logAgent成功-------"
 
-go build -o bin/logAgent  ./logAgent/*.go
+echo "----------开始构建logManager-------"
+cd $ROOTPATH/logManager
+cp -R conf bin/conf
+go build -x -o bin/logManager logManager/manager
+echo "----------构建logManager成功-------"
