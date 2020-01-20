18c6ed2faa767110362eec4b7d75cbd5f1bd715e
yihongzhi <yihongzhi@megvii.com>
2020-01-20 16:23:05 +0800

    manager

--- manager/models/models.go
+++ manager/models/models.go
@@ -5,7 +5,7 @@ import _ "github.com/go-sql-driver/mysql"
 
 func init() {
 	orm.RegisterDriver("mysql", orm.DRMySQL)
-	orm.RegisterDataBase("default", "mysql", "root:peE5RTd2@tcp(10.231.50.30:13306)/log_collect?charset=utf8")
+	orm.RegisterDataBase("default", "mysql", "root:peE5RTd2@tcp(10.231.50.30:3306)/log_collect?charset=utf8")
 	//orm.Debug = true
 }
 
