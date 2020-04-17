5674823109c6cf4c3c1d1d39205a6cad480d524e
723861898@qq.com <723861898@qq.com>
2020-04-17 22:11:22 +0800

    使用命令行组件

--- cmd/logcollect/main.go
+++ cmd/logcollect/main.go
@@ -2,23 +2,37 @@ package main
 
 import (
 	"fmt"
+	"os"
+
 	"github.com/sirupsen/logrus"
 	"github.com/urfave/cli"
-	"os"
 )
 
 func main() {
 	app := cli.NewApp()
-	app.Name = "分布式日志收集"
+	app.Name = "logcollect"
+	app.Description = "分布式日志收集组件"
 	app.Version = "1.0.0"
-	app.Flags = []cli.Flag{
-		cli.StringFlag{Name: "ip"},
-	}
 	app.Commands = []cli.Command{
-		cli.Command{
-			Name: "agent",
+		{
+			Name:        "agent",
+			Description: "服务器收集模块",
 			Action: func(c *cli.Context) {
 				fmt.Println("agent")
+			},
+		},
+		{
+			Name:        "analysis",
+			Description: "日志解析模块",
+			Action: func(c *cli.Context) {
+				fmt.Println("analysis")
+			},
+		},
+		{
+			Name:        "manager",
+			Description: "日志查询管理模块",
+			Action: func(c *cli.Context) {
+				fmt.Println("manager")
 			},
 		},
 	}
