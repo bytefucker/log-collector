37cd2c526e22752edc960c1dc3b2c309dda5f524
yihongzhi <yihongzhi@megvii.com>
2021-01-18 18:34:30 +0800

    构建脚本

--- shell/docker-build-dev.sh
+++ shell/docker-build-dev.sh
@@ -5,4 +5,4 @@ pwd
 echo 开始编译镜像
 docker build -f docker/Dockerfile -t ampregistry:5000/log-collector .
 echo 开始推送镜像
-docker push  ampregistry:5000/log-collector:latest
+docker push ampregistry:5000/log-collector:latest
